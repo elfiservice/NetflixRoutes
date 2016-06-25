20d682a780c98504f6971f98bc3217a626197c82
elfiservice <elfiservice@hotmail.com>
2016-06-25 11:15:57 -0300

    alterado CSS

--- controller/home-index-controller.js
+++ controller/home-index-controller.js
@@ -16,14 +16,3 @@ angular.module('NetflixRouter')
             
         }*/
 		
-//var app = angular.module("NetflixRouter", ["ngResource"]);
-
-function MyCtrl($scope, $resource) {
-			  var TwitterAPI = $resource("http://search.twitter.com/search.json",
-    { callback: "JSON_CALLBACK" },
-    { get: { method: "JSONP" }});
-
-  $scope.search = function() {
-    $scope.searchResult = TwitterAPI.get({ q: $scope.searchTerm });
-  };
-}
